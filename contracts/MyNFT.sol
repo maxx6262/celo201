b1a7e14d74d86a8f779a9729101e78621dfd94cf
LECOUSTRE Maxime <MLECOUSTRE@nexity.fr>
2023-05-08 12:48:45 +0200

    fix compilation issues

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -15,34 +15,40 @@ contract MyNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     constructor() ERC721("MyNFT", "MNFT") {}
 
     function safeMint(address to, string memory uri) public onlyOwner {
-        uint256 tokenId =   _tokenIdCounter.current();
+        uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
-// The following functions are overrides required by Solidity
+    // The following functions are overrides required by Solidity.
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override(ERC721, ERC721Enumerable)
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+    internal virtual
+    override(ERC721, ERC721Enumerable)
     {
-        super._beforeTokenTransfer(from, to, tokenId);
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
     }
 
-    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage)
-        returns (string memory) {
-        super.tokenURI(tokenId);
+    function tokenURI(uint256 tokenId)
+    public
+    view
+    override(ERC721, ERC721URIStorage)
+    returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
+    function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    override(ERC721, ERC721Enumerable)
+    returns (bool)
     {
-        return super.supportsInterfacce(interfaceId);
+        return super.supportsInterface(interfaceId);
     }
 }
