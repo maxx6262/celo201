d4fcdc24def248191e985f008ef8383edb633167
LECOUSTRE Maxime <MLECOUSTRE@nexity.fr>
2023-05-09 20:19:16 +0200

    adding last js files

--- src/App.js
+++ src/App.js
@@ -10,38 +10,38 @@ import coverImg from "./assets/img/nft_geo_cover.png";
 import "./App.css";
 
 const App = function AppWrapper() {
-  const { address, destroy, connect } = useContractKit();
-  const { balance, getBalance } = useBalance();
-  const minterContract = useMinterContract();
+    const { address, destroy, connect } = useContractKit();
+    const { balance, getBalance } = useBalance();
+    const minterContract = useMinterContract();
+}
 
-  return (
-      <>
+return (
+    <>
         <Notification />
         {address ? (
             <Container fluid="md">
-              <Nav className="justify-content-end pt-3 pb-5">
-                <Nav.Item>
-                  <Wallet
-                      address={address}
-                      amount={balance.CELO}
-                      symbol="CELO"
-                      destroy={destroy}
-                  />
-                </Nav.Item>
-              </Nav>
-              <main>
-                <Nfts
-                    name="GEO Collection"
-                    updateBalance={getBalance}
-                    minterContract={minterContract}
-                />
-              </main>
+                <Nav className="justify-content-end pt-3 pb-5">
+                    <Nav.Item>
+                        <Wallet
+                            address={address}
+                            amount={balance.CELO}
+                            symbol="CELO"
+                            destroy={destroy}
+                        />
+                    </Nav.Item>
+                </Nav>
+                <main>
+                    <Nfts
+                        name="GEO Collection"
+                        updateBalance={getBalance}
+                        minterContract={minterContract()}
+                    />
+                </main>
             </Container>
         ) : (
             <Cover name="GEO Collection" coverImg={coverImg} connect={connect} />
         )}
-      </>
-  );
-};
+    </>
+);
 
 export default App;
--- src/components/minter/Cover.js
+++ src/components/minter/Cover.js
@@ -8,12 +8,13 @@ const Cover = ({ name, coverImg, connect }) => {
             <div
                 className="d-flex justify-content-center flex-column text-center "
                 style={{ background: "#000", minHeight: "100vh" }}
-            >
-                <div className="mt-auto text-light mb-5">
+                >
+                <div
+                    className="mt-auto text-light mb-5">
                     <div
-                        className=" ratio ratio-1x1 mx-auto mb-2"
+                        className="ratio ratio-1x1 mx-auto mb-2"
                         style={{ maxWidth: "320px" }}
-                    >
+                        >
                         <img src={coverImg} alt="" />
                     </div>
                     <h1>{name}</h1>
@@ -22,7 +23,7 @@ const Cover = ({ name, coverImg, connect }) => {
                         onClick={() => connect().catch((e) => console.log(e))}
                         variant="outline-light"
                         className="rounded-pill px-3 mt-3"
-                    >
+                        >
                         Connect Wallet
                     </Button>
                 </div>
@@ -33,7 +34,7 @@ const Cover = ({ name, coverImg, connect }) => {
     }
 
     return null;
-};
+}
 
 Cover.propTypes = {
     name: PropTypes.string,
